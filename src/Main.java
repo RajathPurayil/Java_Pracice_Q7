37092c5be865fc20eec1c922a33ea7f044b75cda
Rajath12e <38193362+Rajath12e@users.noreply.github.com>
2024-03-17 23:50:16 -0400

    Update comment

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    /*    (Compute the volume of a cylinder) Write a program that reads in the radius
+    and length of a cylinder and computes the area and volume using the following formulas:
+        area = radius * radius * pi
+        volume = area * length
+
+                */
+
+        //Creating Scanner object
+        Scanner sc = new Scanner(System.in);
+        final double PI = 3.14159265359;
+        double area,volume;
+
+        // Prompt user to enter the radius and length of a cylinder
+
+        System.out.println("Enter the radius");
+        double radius= sc.nextDouble();
+        System.out.println("Enter the length");
+        double length=sc.nextDouble();
+
+    // Comput the area and volume
+
+            area = radius*PI;
+            volume = area*length;
+
+            // Display result
+
+        System.out.println("Area of cylider is  "+area);
+        System.out.println("Volume of cylider is  "+volume);
+
+
+
+
+
+
+
+
+
+
     }
 }
